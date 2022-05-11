f18a8da32851601c145c7c976aaf87ed351e7e5e
sandra <sandravahtel4@gmail.com>
2022-05-11 20:09:16 +0200

    edit4

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ alert (age);
 
 nom="Malki";
 alert (nom);
+
+if (age > 17) { 
+    alert ("vous etes majeur !");
+}
+if (age < 18) { 
+    alert ("vous etes mineur !");
+}
+for (i=0;i<10;i++) { 
+alert ("je suis heureux");
+}
+
+function afficher_alert () { 
+alert ("je suis dans la function");
+}
+
+afficher_alert ()
---
+++ script2.js
@@ -0,0 +1,3 @@
+function toto() { 
+alert ("vous avez clique sur le bouton");
+}
