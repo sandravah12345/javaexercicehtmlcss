aafbea27fa06b4301c5559c1ae31c364bfe53dd5
sandra <sandravahtel4@gmail.com>
2022-05-11 15:33:34 +0200

    edit3

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert ("comme vas tu")
+alert ("welcome")
+document.getElementById("madiv").innerHTML="le nouveau text"
+document.getElementById("monparagraphe").innerHTML="le nouveau paragraphe"
+
+age=18;
+alert (age);
+
+nom="Malki";
+alert (nom);
